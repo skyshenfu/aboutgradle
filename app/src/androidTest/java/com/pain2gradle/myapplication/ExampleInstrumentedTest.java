5f5ea4d39e99bb1a23f59b9975a8915c30b845d0
lude <zhangty@all-help.com>
2017-11-04 20:09:13 +0800

    gradle相关demo使用完成

--- app/src/androidTest/java/com/pain2gradle/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pain2gradle/myapplication/ExampleInstrumentedTest.java
@@ -1,9 +1,15 @@
 package com.pain2gradle.myapplication;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,13 +20,29 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+/**
+ * 编写好该类之后 直接gradlew connectedAndroidTest即可在模拟器上运行
+ */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
+   /* @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.pain2gradle.myapplication", appContext.getPackageName());
+    }*/
+   //添加需要测试的Activity
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule=new ActivityTestRule<MainActivity>(MainActivity.class);
+    //必须@Test 如果进行UI的操作还要@uiTest
+    @UiThreadTest
+    @Test
+    public void vaildSomething(){
+        mainActivityTestRule.getActivity().findViewById(R.id.textview).performClick();
+
     }
+
 }
--- app/src/main/java/com/pain2gradle/myapplication/MainActivity.java
+++ app/src/main/java/com/pain2gradle/myapplication/MainActivity.java
@@ -2,13 +2,24 @@ package com.pain2gradle.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //充当一次提交
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textview);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "here", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
