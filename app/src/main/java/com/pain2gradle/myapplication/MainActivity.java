ff02aad778b8e6c4c30d1ca6adb6aea44b8d4eaa
lude <zhangty@all-help.com>
2017-11-04 15:14:37 +0800

    提交2次

--- app/src/main/java/com/pain2gradle/myapplication/MainActivity.java
+++ app/src/main/java/com/pain2gradle/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //充当一次提交
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
